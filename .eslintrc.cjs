a84a9aeaa0f5ac8b750c0f963aa2783efce6eb9e
naporin0624 <naporin0624@gmail.com>
2023-04-21 16:16:56 +0900

    🎉 initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  extends: ["@naporin0624/eslint-config"],
+  rules: {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "**/*.dev.ts",
+          "**/*.test.ts",
+          "**/*.test.tsx",
+          "./*.config.js",
+          "./*.config.cjs",
+          "./*.config.mjs",
+          "**/*.dev.mjs",
+          "./*.config.ts",
+          "**/*.stories.tsx",
+          "**/*.story.tsx",
+          "vitest-*",
+        ],
+      },
+    ],
+  },
+
+  overrides: [
+    {
+      files: ["types/**/*.d.ts"],
+      rules: {
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/consistent-type-imports": "off"
+      },
+    },
+  ],
+};
+
+module.exports = config;
